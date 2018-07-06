de4ffbea6a2f5417ad9548a682f36e05ba424c28
garubi <sgaruti@gmail.com>
2018-07-06 15:27:25 +0200

    Set the device's USB name. cloese issue #10

---
+++ name.c
@@ -0,0 +1,19 @@
+// To give your project a unique name, this code must be
+// placed into a .c file (its own tab).  It can not be in
+// a .cpp file or your main sketch (the .ino file).
+
+#include "usb_names.h"
+
+// Edit these lines to create your own name.  The length must
+// match the number of characters in your custom name.
+
+#define MIDI_NAME   {'U','B','I','S','t','a','g','e',' ', 'D','9', ' ', 'D','r','a','w','b','a','r','s', ' ', 'C','o','n','t','r','o','l','l','e','r'}
+#define MIDI_NAME_LEN  31
+
+// Do not change this part.  This exact format is required by USB.
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+        2 + MIDI_NAME_LEN * 2,
+        3,
+        MIDI_NAME
+};
